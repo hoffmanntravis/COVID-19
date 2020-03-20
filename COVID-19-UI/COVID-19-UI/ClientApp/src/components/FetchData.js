2bb0b86eb5255262a374fd7e67ebaf5b1bd06c7f
Travis Hoffmann <hoffmann.travis@gmail.com>
2020-03-19 21:01:14 -0600

    Added counts to legends and got graph transparency working

--- COVID-19-UI/COVID-19-UI/ClientApp/src/components/FetchData.js
+++ COVID-19-UI/COVID-19-UI/ClientApp/src/components/FetchData.js
@@ -24,12 +24,12 @@ export class FetchData extends Component {
             : null;
 
         var CountryData = this.state.CovidData;
-        var i;
         var ConfirmedDataPoints = [];
         var DeathsDataPoints = [];
         var RecoveredDataPoints = [];
-        //{ x: new Date("2017- 01- 01"), y: 84.927 },
-        for (i = 0; i < CountryData.length; i++) {
+        var Dates = [];
+
+        for (var i = 0; i < CountryData.length; i++) {
             var confirmed = { x: null, y: null };
             confirmed.x = new Date(CountryData[i].Key);
             confirmed.y = CountryData[i].Value.Confirmed;
@@ -44,44 +44,73 @@ export class FetchData extends Component {
             recovered.x = new Date(CountryData[i].Key);
             recovered.y = CountryData[i].Value.Recovered;
             RecoveredDataPoints.push(recovered);
+            
+            var TotalConfirmed = ConfirmedDataPoints[ConfirmedDataPoints.length - 1].y;
+            var TotalDeaths = DeathsDataPoints[DeathsDataPoints.length - 1].y;
+            var TotalRecovered = RecoveredDataPoints[RecoveredDataPoints.length - 1].y;
+            
         }
 
         var state = {
-            labels: ['January', 'February', 'March',
-                'April', 'May'],
             datasets: [
                 {
-                    label: 'Rainfall',
-                    fill: false,
+                    label: `Confirmed: ${TotalConfirmed}`,
+                    fill: true,
                     lineTension: 0.5,
-                    backgroundColor: 'rgba(75,192,192,1)',
+                    backgroundColor: 'rgba(102,178,255,.5)',
                     borderColor: 'rgba(0,0,0,1)',
                     borderWidth: 2,
-                    data: [65, 59, 80, 81, 56]
+                    data: ConfirmedDataPoints
+                },
+                {
+                    label: `Deaths: ${TotalDeaths}`,
+                    fill: true,
+                    lineTension: 0.5,
+                    backgroundColor: 'rgba(180,0,0,.5)',
+                    borderColor: 'rgba(0,0,0,1)',
+                    borderWidth: 2,
+                    data: DeathsDataPoints
+                },
+                {
+                    label: `Recovered: ${TotalRecovered}`,
+                    fill: true,
+                    lineTension: 0.5,
+                    backgroundColor: 'rgba(0,128,0,.5)',
+                    borderColor: 'rgba(0,0,0,1)',
+                    borderWidth: 2,
+                    data: RecoveredDataPoints
                 }
             ]
+        }
+        var options = {
+            responsive: true,
+            title: {
+                display: true,
+                text: 'COVID-19 Status',
+                fontSize: 20
+            },
+            legend: {
+                display: true,
+                position: 'right'
+            },
+            scales: {
+                xAxes: [{
+                    type: 'time',
+                }]
+            }
         }
 
         return (
             <div>
                 <Line
                     data={state}
-                    options={{
-                        title: {
-                            display: true,
-                            text: 'Average Rainfall per month',
-                            fontSize: 20
-                        },
-                        legend: {
-                            display: true,
-                            position: 'right'
-                        }
-                    }}
+                    options={options}
                 />
-            </div>
+            </div >
         );
 
     }
+
 
     async GetCovidData() {
         //https://localhost:44353/api/country?Country=Mainland%20China&Province=Anhui
